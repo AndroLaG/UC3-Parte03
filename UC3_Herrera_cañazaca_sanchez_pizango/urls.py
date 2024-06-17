f5c667e8a23495f08636fdd5fdc365f63de4f5ff
Andres Sanchez <jugernaru2003@gmail.com>
2024-06-17 17:53:45 -0500

    UC3_Parte03_ApellidoDelEstudiante03

--- UC3_Herrera_cañazaca_sanchez_pizango/urls.py
+++ UC3_Herrera_cañazaca_sanchez_pizango/urls.py
@@ -5,5 +5,11 @@ from miapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name = "index"),
+    path('inicio/', views.index, name = "inicio"),
     path('saludo/',views.saludo, name = "saludo"),
+    path('rango/',views.primos_entre_a_y_b,name="rango"),
+
+
 ]
+ 
--- miapp/views.py
+++ miapp/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render,HttpResponse
 
 
-# Create your views here.
-def saludo(request):
-    mensaje ="""
-        <h1>LENGUAJE DE PROGRAMACIÓN 3</h1>
-        <ol class="list-group list-group-numbered">
-  <li class="list-group-item">Hans Herrera Castillo</li>
-  <li class="list-group-item">Emily Cañazaca Vasquez</li>
-  <li class="list-group-item">Andres Sanchez Tucta</li>
-    <li class="list-group-item">Flor Pizango Velezmoro</li>
-
-</ol>
+def index(request):
+    mensaje="""
+        <h1>Inicio</h1>
     """
     return HttpResponse(mensaje)
+
+
+def saludo(request):
+    mensaje ="""
+        <h1>Bienvenidos al curso</h1>
+        <h2>Mg. Flor Elizabeth Cerdán León</h2>
+        <h3>Todo lo puedo en Cristo que me fortalece</h3>
+    """
+    return HttpResponse(layout + mensaje)
+
+
+
+
+def primos_entre_a_y_b(a, b):
+    if a > b:
+        a, b = b, a
+
+    def es_primo(n):
+        if n <= 1:
+            return False
+        if n == 2:
+            return True
+        if n % 2 == 0:
+            return False
+        for i in range(3, int(n**0.5) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
+
+    primos = [num for num in range(a, b + 1) if es_primo(num)]
+    return primos
+
+# Valores de prueba
+a = 1
+b = 20
+print(primos_entre_a_y_b(a, b))
+
