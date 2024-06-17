54c35fc1762f8caffe211cca6588c3da1799d4e1
Emily Cañazaca <emi41lorena@gmail.com>
2024-06-17 17:13:33 -0500

    UC3_Parte02_Cañazaca

--- miapp/views.py
+++ miapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+
 
 # Create your views here.
+def saludo(request):
+    mensaje ="""
+        <h1>LENGUAJE DE PROGRAMACIÓN 3</h1>
+        <ol class="list-group list-group-numbered">
+  <li class="list-group-item">Hans Herrera Castillo</li>
+  <li class="list-group-item">Emily Cañazaca Vasquez</li>
+  <li class="list-group-item">Andres Sanchez Tucta</li>
+    <li class="list-group-item">Flor Pizango Velezmoro</li>
+
+</ol>
+    """
+    return HttpResponse(mensaje)
